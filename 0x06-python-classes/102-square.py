649a9ca4ed7eb02e382415bfd6dbdf7d06834a23
nimba005 <nimbaoyugi@gmail.com>
2023-08-29 12:47:48 -0700

    python-classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 class Square:
-    def __init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0):
         self.size = size
-        self.position  = position
     @property
     def size(self):
-        """ Get/set the current size of the squre. """
         return (self.__size)
     @size.setter
     def size(self, value):
@@ -15,38 +13,24 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    @property
-    def position(self):
-        """ Get/set the current position of the square. """
-        return (self.__position)
-    @position.setter
-    def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
     def area(self):
-        """ Return the current area of the square. """
+        """ Return the current area of the square."""
         return (self.__size * self.__size)
-    def my_print(self):
-        """ Print the square with the # character. """
-        if self.__size == 0:
-            print("")
-            return
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
-    def __str__(self):
-        """ Define print() representation of a square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+    def __eq__(self, other):
+        """ Define the == comparison to a square."""
+        return self.area() == other.area()
+    def __ne__(self, other):
+        """ Define the != comparison to a square. """
+        return self.area() != other.area()
+    def __it__(self, other):
+        """ Define the < comparison to a square."""
+        return self.area() < other.area()
+    def __le__(self, other):
+        """ Define the <= comparison to a square."""
+        return self.area() <= other.area()
+    def __gt__(self, other):
+        """ Define the > comparison to a square."""
+        return self.area() > other.area()
+    def __ge__(self, other):
+        """ Define the >= comparison to a square."""
+        return self.area() >= other.area()
