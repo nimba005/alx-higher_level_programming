95f2b70c2896106f0226dbadfa9b1dd8388da3a5
nimba005 <nimbaoyugi@gmail.com>
2023-09-16 01:59:41 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,4 +18,35 @@ class Square(Rectangle):
     def __str__(self):
         """STR DUNDER METHOD"""
         str_s = "[squaee]"
-        str_id = 
+        str_id = "({})".format(self.0x0C-python-almost_a_circleid)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_w = "{}/{}".format(self.width, self.height)
+        return str_s + str_id + str_xy + str_w
+    @property
+    def size(self):
+        """GET THE SIZE"""
+        return self.width
+    @size.setter
+    def size(self, value):
+        """UPDATE THE SIZE"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ STR DUNDER METHOD """
+        str_rectangle = "[Squae]"
+        str_id = "({})".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+        return str_rectangle + str_id + str_xy + str_size
+    def update(self, *args, **kwargs):
+        """UPDATE THE SQUARE
+        Args:
+           *args (ints): New attribute value.
+               - 1st argument representing id attribute
+               - 2nd argument representing size attribute
+               - 3rd argument representing x attribute
+               - 4th argument representing y attribute
+           **kwargs (dict): New key/value pairs of attributes
+        """
+
